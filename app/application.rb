2544d3d3cf6636a858bbab3be1ac88a95479a2b2
Sev Furneaux <sevfurneaux@gmail.com>
2015-05-23 16:38:32 +0100

    Added Image component

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,11 @@ require "react/native"
 
 class App
   include React::NativeComponent
-    
+
   before_mount do
     puts "Hello!"
   end
-  
+
   def render
     styles = React::StyleSheet.new({
       container: {
@@ -29,13 +29,14 @@ class App
         marginBottom: 5,
       },
     })
-    
+
     present(View, {style: styles.container}) do
        present(Text, {style: styles.welcome}) { "Welcome to React Native!💖" }
        present(Text, {style: styles.instructions}) { "To get started, edit app/application.rb" }
        present(Text, {style: styles.instructions}) do
          "Press Cmd+R to reload,\nCmd+Control+Z for dev menu"
        end
+       present(Image, {source: 'https://media4.giphy.com/media/ODy29v7YAJrck/200_s.gif'})
     end
   end
 end
